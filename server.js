01a432cfd946a94aeaf0b6c5555afd0000037829
Sid <siddharth.god@gmail.com>
2014-03-03 11:17:58 -0500

    Node app - Some helpers

--- server.js
+++ server.js
@@ -14,3 +14,24 @@ function sendFile(response, filePath, fileContents) {
     response.writeHead(200, {'Content-Type': mime.lookup(path.basename(filePath))});
     response.end(fileContents);
 }
+
+function serveStatic(response, cache, absPath) {
+    if(cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function(exists) {
+            if(exists) {
+                fs.readFile(absPath, function(err, data) {
+                    if(err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);   
+            }
+        });
+    }
+}
